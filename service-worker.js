28433d7b7bcc78e55ee3358fe5821a1bb16188e4
ljcamargog <lourivaljuniorcamargogomes@gmail.com>
2026-08-28 08:33:29 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // V4.3 - atualização de ícone iPhone
-const C='curral-v42';
-const A=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
+const C='curral-v44';
+const A=['./','index.html','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
